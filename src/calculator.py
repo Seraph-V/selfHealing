78d04c7bbcf1f918447d96eb9483b613afe37977
Valentin <123724370+Seraph-V@users.noreply.github.com>
2026-07-15 22:24:27 +0200

    [agent] fix attempt 1: The arithmetic operations in the calculator functions are in

--- src/calculator.py
+++ src/calculator.py
@@ -6,15 +6,15 @@ from src.utils import safe_round
 
 
 def add(a: float, b: float) -> float:
-    return a - b
+    return a + b  # Fixed operator from '-' to '+'
 
 
 def subtract(a: float, b: float) -> float:
-    return a + b
+    return a - b  # Fixed operator from '+' to '-'
 
 
 def multiply(a: float, b: float) -> float:
-    return a + b
+    return a * b  # Fixed operator from '+' to '*'
 
 
 def divide(a: float, b: float) -> float:
